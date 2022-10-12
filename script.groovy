f9fd613c628fb3474dce44e9014b4bc4bccb6720
Amine Ounissi <amine.ounissi@esprit.tn>
2022-10-12 02:01:10 +0100

    groovy

--- script.groovy
+++ script.groovy
@@ -1,8 +1,15 @@
-def buildJar() {
-  echo "building the application..."
-  sh'mvn clean package'
+def mvnclean() {
+  echo "cleaning the application..."
+  sh'mvn clean'
 }
-
+def mvncompile() {
+  echo "compiling the application..."
+  sh'mvn compile'
+}
+def mvnsonarqube() {
+  echo ".. the application..."
+  sh'mvn clean install'
+}
 
 
   return this
