856a9f0cbfa948410ce68fdb092e82ae55a6c6ca
Amine Ounissi <amine.ounissi@esprit.tn>
2022-10-16 14:42:42 +0100

    Junit

--- src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTest.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTest.java
@@ -22,35 +22,35 @@ public class StockServiceImpTest {
     @Order(1)
     public void testSaveStock(){
 
-        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(100).qteMin(10).build());
+        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(90).qteMin(7).build());
         Assertions.assertNotNull(s);
     }
     @Test
     @Order(2)
     public void testRetrieveAllStocks() {
         int listStocks = StockServiceImpl.retrieveAllStocks().size();
-        StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(100).qteMin(10).build());
+        StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(90).qteMin(7).build());
         Assertions.assertEquals(listStocks+1, StockServiceImpl.retrieveAllStocks().size());
     }
     @Test
     @Order(3)
     public void testRetrieveOneStock() {
-        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(100).qteMin(10).build());
+        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(90).qteMin(7).build());
 
         Assertions.assertEquals(s.getIdStock(),StockServiceImpl.retrieveStock(s.getIdStock()).getIdStock());
     }
     @Test
     @Order(4)
     public void testUpdateStock() {
-        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(100).qteMin(10).build());
-        s.setQteMin(5);
-        Assertions.assertEquals(5,StockServiceImpl.updateStock(s).getQteMin());
+        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(90).qteMin(7).build());
+        s.setQteMin(4);
+        Assertions.assertEquals(4,StockServiceImpl.updateStock(s).getQteMin());
     }
     @Test
     @Order(5)
     public void testDeletetock(){
 
-        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(100).qteMin(10).build());
+        Stock s= StockServiceImpl.addStock(Stock.builder().libelleStock("stock test").qte(90).qteMin(7).build());
         StockServiceImpl.deleteStock(s.getIdStock());
         Assertions.assertNull(StockServiceImpl.retrieveStock(s.getIdStock()));
 
