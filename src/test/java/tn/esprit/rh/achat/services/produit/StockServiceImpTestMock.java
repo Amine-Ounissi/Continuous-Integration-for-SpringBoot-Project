c688bb032221c4d35db1686c77e91511e55ac9b7
Amine Ounissi <amine.ounissi@esprit.tn>
2022-10-18 18:24:40 +0100

    update

--- src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
@@ -26,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@RunWith(MockitoJUnitRunner.class)
+//@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class StockServiceImpTestMock {
