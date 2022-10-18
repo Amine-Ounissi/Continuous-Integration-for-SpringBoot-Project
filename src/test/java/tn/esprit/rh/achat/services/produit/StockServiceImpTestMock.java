a372a813ef17e33d66c82d871c54e301594f65a7
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-10-18 18:27:06 +0100

    Update StockServiceImpTestMock.java

--- src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+//import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import org.springframework.boot.test.context.SpringBootTest;
