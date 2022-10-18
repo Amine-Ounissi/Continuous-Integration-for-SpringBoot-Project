7f004bbf7b8edffa2470dbc2e57a54a7c120d605
Amine Ounissi <73407173+Amine-Ounissi@users.noreply.github.com>
2022-10-18 18:21:20 +0100

    Update StockServiceImpTestMock.java

--- src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
+++ src/test/java/tn/esprit/rh/achat/services/produit/StockServiceImpTestMock.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class StockServiceImpTestMock {
 @Mock
 StockRepository stockRepository;
